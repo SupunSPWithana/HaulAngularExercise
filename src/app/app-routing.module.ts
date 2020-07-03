a8e7c7b377d068243507001772597adeb83b8a70
fakename <fakeemail@gmail.com>
2020-07-03 14:15:55 +0530

    First step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DirectiveComponent } from './dashboard/directive/directive.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full"},
+  { path : "login" , component : LoginComponent },
+  { path : "dashboard" , component : DashboardComponent },
+  { path : "dashboard/directive" , component : DirectiveComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
